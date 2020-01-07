6fb5ac9f176bf2ab715b04f65a97fc7e1d772405
Turan Ketene <turank97@gmail.com>
2020-01-06 20:47:25 -0800

    added code to server.js file

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-var test = blah blah 
+// Declaring Dependencies
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+const apiRoutes = require("./routes/index");
+const PORT = process.env.PORT || 3001;
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Setting up build file as the static(for heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+// Defining API routes
+app.use("/api", apiRoutes);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/favoritecars");
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
