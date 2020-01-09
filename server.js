3f509c9020c0bf1f59c9afe2c86bf48a15d143ea
Turan Ketene <turank97@gmail.com>
2020-01-08 20:51:13 -0800

    added code to controllers folder and api key

--- server.js
+++ server.js
@@ -26,3 +26,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/favoritecars");
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
+  
+  // API Key: QSbmSOpWByXGw6PBBUy4wa7HS7EpN8EQ
