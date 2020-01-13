0c63a49c31f9a01930126d9ab5d5d96dc80ecc15
Turan Ketene <turank97@gmail.com>
2020-01-12 23:33:21 -0800

    added some functionality

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron text-center">
+            <h1>Auto Guru</h1>
+            <h3>Search for your car of interest</h3>
+        </div>
+    );
+}
+
+export default Jumbotron;
