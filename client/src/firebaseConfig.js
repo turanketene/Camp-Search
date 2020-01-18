02cac07694eb9b271718d17d87441023f276de35
Naldo Tumundo <naldo_j3@yahoo.com>
2020-01-18 13:49:20 -0800

    add form and login with firebase

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyCMS2aKWCzeYoOQ32IRncXYNo139cEYS8o",
+    authDomain: "campsearch-3f400.firebaseapp.com",
+    databaseURL: "https://campsearch-3f400.firebaseio.com",
+    projectId: "campsearch-3f400",
+    storageBucket: "campsearch-3f400.appspot.com",
+    messagingSenderId: "406169783106",
+    appId: "1:406169783106:web:08913bc09d6045158d96db",
+    measurementId: "G-SFW2XRVHGS"
+  };
+  export default config;
