2bcada79b8224c68715b429897f135dd14d31c5f
Turan Ketene <turank97@gmail.com>
2020-01-18 13:59:33 -0800

    added new pages

---
+++ client/src/pages/Favorites/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import API from "../../utils/API";
+import Cards from "../../components/Cards"
+import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Cards from "../../components/Cards"
+import Container from '@material-ui/core/Container';
+import LinearProgress from '@material-ui/core/LinearProgress';
