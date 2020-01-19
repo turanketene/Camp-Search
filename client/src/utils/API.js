1cfac5a86e1c95fc43aa02cdf543a3e48d3aa276
Turan Ketene <turank97@gmail.com>
2020-01-19 11:56:44 -0800

    worked on home page, and APIs

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,13 +2,25 @@ import axios from "axios";
 
 const BaseUrl = "https://www.googleapis.com/books/v1/volumes?q="
 
+// Getting the campgrounds by their ID
+
+function getCampgroundById(parkCode) {
+  return axios
+      .get(`https://developer.nps.gov/api/v1/campgrounds?parkCode=${parkCode}&api_key=O4VdhmolNStlPLj2bo2DfPKWks3F8J9xfihpGqTf`)
+      .then(result => result.data);
+}
 export default {
-  // calls googlbooks api and retrieve books based on user input
-  searchBooks: (query) => axios.get(BaseUrl + query),
-  // get all books saved in db
-  getBooks: () => axios.get("/api/books"),
-  // saves a book to the db
-  saveBook: (bookData) => axios.post("/api/books", bookData),
-  // deletes a book with the given id
-  deleteBook: (id) => axios.delete("api/books/" + id)
-};
+  // API request from National Park Service 
+  getCampground: function (query) {
+      return axios.get(`https://developer.nps.gov/api/v1/campgrounds?q=${query}&api_key=O4VdhmolNStlPLj2bo2DfPKWks3F8J9xfihpGqTf`);
+  },
+  // API request from OpenWeather 
+  getWeather: function (lat, lon, cnt) {
+      return axios.get(`api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=${cnt}`)
+  },
+  // API request from Hiking Project
+  getTrails: function (lat, lng) {
+      return axios.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=10&key=200542336-9cc42e1d5b620d5f44636b1bd9dc58f3`)
+      .then(result => result.data);
+  }
+}
