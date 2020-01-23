7e6ffabb43ebee932caa2a78240ddc29db923cf5
Turan Ketene <turank97@gmail.com>
2020-01-22 18:14:40 -0800

    deployment stuff

---
+++ client/build/precache-manifest.7fe854d9d935337392dc3608542bee20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8da96f629f15eada89f29ce9bd450d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a704710e7458bceaf6b",
+    "url": "/static/css/main.2df4ed4a.chunk.css"
+  },
+  {
+    "revision": "ce1d91d11453e7c23c6e",
+    "url": "/static/js/2.0ac3fddc.chunk.js"
+  },
+  {
+    "revision": "947d71d9788fd87037415f4aa5284067",
+    "url": "/static/js/2.0ac3fddc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a704710e7458bceaf6b",
+    "url": "/static/js/main.bf07ef06.chunk.js"
+  },
+  {
+    "revision": "2543631445c241be0be8",
+    "url": "/static/js/runtime-main.e9074294.js"
+  },
+  {
+    "revision": "b87d541f816b9aafa5d38267c72e8ae7",
+    "url": "/static/media/logo.b87d541f.png"
+  }
+]);
